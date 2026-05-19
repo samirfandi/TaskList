6b3245d4c4a4d88b1c765e912c12300735ce7d50
Fandi <samirunvlille@gmail.com>
2026-05-19 19:25:07 +0200

    Organize task's items

--- src/Task.js
+++ src/Task.js
@@ -1,5 +1,32 @@
+import "./Task.css";
+import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
+import ModeEditOutlineOutlinedIcon from "@mui/icons-material/ModeEditOutlineOutlined";
+import CheckOutlinedIcon from "@mui/icons-material/CheckOutlined";
+
 export default function Task() {
-  return <div>
-    
-  </div>;
+  return (
+    <div className="Task">
+      <div className="titles">
+        <h3> Title</h3>
+        <p> SubTitle</p>
+      </div>
+
+      <div className="icons">
+        <button className="iconButton">
+          <DeleteOutlinedIcon color="secondary" sx={{ fontSize: 25 }}>
+            {" "}
+          </DeleteOutlinedIcon>
+        </button>
+        <button className="iconButton">
+          <ModeEditOutlineOutlinedIcon
+            sx={{ fontSize: 25 }}
+            color="primary"
+          ></ModeEditOutlineOutlinedIcon>
+        </button>
+        <button className="iconButton">
+          <CheckOutlinedIcon sx={{ fontSize: 25 }} color=""></CheckOutlinedIcon>
+        </button>
+      </div>
+    </div>
+  );
 }
