67f153aa941a859be8801a28a72662c6f858bfb9
Fandi <samirunvlille@gmail.com>
2026-06-09 21:18:34 +0200

    Implement Tasks context

--- src/contexts/TasksContext.js
+++ src/contexts/TasksContext.js
@@ -1,3 +1,16 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
+import tasksReducer from "../reducers/tasksReducer";
 
 export const TasksContext = createContext([]);
+
+const TasksProvider = ({ children }) => {
+  const [tasks1, dispatch1] = useReducer(tasksReducer, []);
+  console.log("tasksssPovider1111 =", tasks1);
+  return (
+    <TasksContext.Provider value={{ tasks1: tasks1, dispatch1: dispatch1 }}>
+      {children}
+    </TasksContext.Provider>
+  );
+};
+
+export default TasksProvider;
